9f15093456b0a554294195ad4267b4c6d9d82f19
Fugazzii <sichinavailia@gmail.com>
2024-04-04 16:48:10 +0400

    Forked circom utils and build circuit

--- tests/minimal-demo.ts
+++ tests/minimal-demo.ts
@@ -1,18 +1,15 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { StealthCash } from "../target/types/stealth_cash";
-import { assert } from "chai";
-import { loadKeypair, generateStateAccount, lamportsToSol } from "./utils";
+// import * as anchor from "@coral-xyz/anchor";
+// import { Program } from "@coral-xyz/anchor";
+// import { StealthCash } from "../target/types/stealth_cash";
+// import crypto from "node:crypto";
+// import { loadKeypair, generateStateAccount } from "./utils";
+// async function main() {
+//     anchor.setProvider(anchor.AnchorProvider.env());
 
-describe("Minimal Demo", async () => {
-    anchor.setProvider(anchor.AnchorProvider.env());
+//     const { connection } = anchor.getProvider();
+//     const program = anchor.workspace.StealthCash as Program<StealthCash>;
+//     const developerKeypair = loadKeypair("wallets/w1.json");
+//     const stateAccountKeypair = await generateStateAccount(connection, "wallets/state.json", developerKeypair);
 
-    const { connection } = anchor.getProvider();
-    const program = anchor.workspace.StealthCash as Program<StealthCash>;
-    const developerKeypair = loadKeypair("wallets/w1.json");
-    const stateAccountKeypair = await generateStateAccount(connection, "wallets/state.json", developerKeypair);
 
-    it("Testing Deposit", async () => {
-        
-    });
-});
+// }
